72ad4b546c8b21865c9132a31f7b82b01002c461
YunusDemircan <yunusdemircan@gmail.com>
2020-03-15 16:52:17 -0400

    fourth commitfrom local branch

--- src/gitPackageOne/GitDay3.java
+++ src/gitPackageOne/GitDay3.java
@@ -4,7 +4,6 @@ public class GitDay3 {
 
 	public static void main(String[] args) {
 		System.out.println("My first changes on local master");
-		
 		System.out.println("This is my feature branch");
 		System.out.println("My second push from local feature branch");
 		System.out.println("My third push from local feature branch");
